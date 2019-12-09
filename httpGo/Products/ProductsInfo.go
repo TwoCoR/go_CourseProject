904a3cc882edb7bc53c5ad53a05d17af57dd5814
TwoCoR <47568642+TwoCoR@users.noreply.github.com>
2019-12-09 12:55:36 +0300

    Add files via upload

---
+++ httpGo/Products/ProductsInfo.go
@@ -0,0 +1,15 @@
+package Products
+
+type Product struct{
+	id int
+	item string
+	company string
+	price int
+	amount int
+}
+type Info struct {
+	id int
+	company string
+	information string
+	rating int
+}
